ffaea8426e1aa87e63a11600cf5dd79bbe067b9c
safin33221 <safin33221@gmail.com>
2026-02-12 22:17:59 +0600

    create dashboard page architecture

---
+++ src/app/(Dashboard)/layout.tsx
@@ -0,0 +1,39 @@
+export const dynamic = 'force-dynamic';
+
+
+import { getCookies } from "@/services/auth/tokenHandler";
+import { redirect } from "next/navigation";
+
+export default async function layout({ children }: { children: React.ReactNode }) {
+
+
+    const accessToken = await getCookies("accessToken")
+    if (!accessToken) {
+        redirect("/")
+
+    }
+    return (
+        <div className="h-screen flex overflow-hidden">
+            {/* Sidebar */}
+            <aside className="w-64 h-full overflow-y-auto border-r max-md:hidden">
+                <h1>Side bar</h1>
+                {/* <DashboardSidebar /> */}
+            </aside>
+
+            {/* Main Area */}
+            <div className="flex flex-1 flex-col overflow-hidden">
+                {/* Navbar */}
+                <div className="shrink-0">
+                    {/* <DashboardNavbar /> */}
+                    <h1>Dashboard navbar</h1>
+                </div>
+
+                {/* Page Content */}
+                <main className="flex-1 overflow-y-auto p-6 mt-16">
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
+}
+
